0e276ef9a834df4f5f2c7f6147f69353b49a4368
Alex.k <Alex.k@ramotion.com>
2016-01-27 14:54:30 +0300

    added clean project

---
+++ CircleMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CircleMenu
+//
+//  Created by Alex K. on 27/01/16.
+//  Copyright © 2016 Alex K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
