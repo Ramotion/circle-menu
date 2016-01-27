5a571515da42b141ec66b77518f2fe3823b70e04
Alex.k <Alex.k@ramotion.com>
2016-01-27 15:18:26 +0300

    added  circle menu

---
+++ CircleMenuLib/CircleMenu.swift
@@ -0,0 +1,157 @@
+//
+//  CircleMenu.swift
+//  ButtonTest
+//
+//  Created by Alex K. on 14/01/16.
+//  Copyright © 2016 Alex K. All rights reserved.
+//
+
+import UIKit
+
+// MARK: helpers
+
+@warn_unused_result
+public func Init<Type>(value: Type, @noescape block: (object: Type) -> Void) -> Type {
+    block(object: value)
+    return value
+}
+
+// MARK: CircleMenu
+public class CircleMenu: UIButton {
+    
+    // MARK: properties
+    
+    @IBInspectable var buttonsCount: Int = 3
+    @IBInspectable var distance: Float = 100 // distance betwen center button and buttons
+    
+    lazy var buttons: [CircleMenuButton] = {
+        var buttons = [CircleMenuButton]()
+        
+        
+        let step: Float = 360.0 / Float(self.buttonsCount)
+        for index in 0..<self.buttonsCount {
+            
+            var angle: Float = Float(index) * step
+            let button = Init(CircleMenuButton(
+                size: self.bounds.size,
+                circleMenu: self,
+                distance:Float(self.bounds.size.height/2.0),
+                angle: angle)) {
+                    
+                $0.tag = index
+                $0.addTarget(self, action: "buttonHandler:", forControlEvents: UIControlEvents.TouchUpInside)
+                $0.alpha = 0
+            }
+            buttons.append(button)
+        }
+        
+        return buttons
+    }()
+    
+    // MARK: life cicle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.addActions()
+    }
+    
+    // MARK: create 
+    
+    private func createMovingObject(
+        size: CGSize,
+        distance: Float,
+        angle: Float,
+        duration: Double,
+        additionAngle: Float) {
+            let object = CircleMenuButton(size: size, circleMenu: self, distance: distance, angle: angle)
+            object.rotationLayerAnimation(angle + 360.0 + additionAngle, duration: duration)
+    }
+    
+    // MARK: configure
+    
+    private func addActions() {
+        self.addTarget(self, action: "onTap", forControlEvents: UIControlEvents.TouchUpInside)
+    }
+    
+    // MARK: helpers
+    
+    public func buttonsIsShown() -> Bool {
+        for button in buttons {
+            if button.alpha == 0 {
+                return false
+            }
+        }
+        return true
+    }
+    
+    // MARK: actions
+    func onTap() {
+        buttonsAnimationShow(isShow: !buttonsIsShown())
+    }
+    
+    func buttonHandler(sender: CircleMenuButton) {
+
+        let duration = 0.5
+        if let container = sender.superview { // rotation
+            createMovingObject(
+                sender.frame.size,
+                distance: distance,
+                angle: container.angleZ,
+                duration: duration,
+                additionAngle: 80)
+        }
+        
+        sender.tapAnimation(byAngle: 90, duration: duration + 0.2, distance: distance) // rotation animation
+        
+        // hide buttons
+        let step = duration / Double(buttons.count + 1)
+        for index in 0..<buttons.count {
+            if index == sender.tag {
+                continue
+            }
+            
+            let animationIndex = index < sender.tag ? buttons.count + (index - sender.tag) : index - sender.tag
+            let delay = Double(step) * Double(animationIndex)
+            buttons[index].hideAnimation(self.bounds.size.width / CGFloat(2.0), duration: 0, delay:delay)
+        }
+    }
+    
+    // MARK: animations
+    
+    private func buttonsAnimationShow(isShow isShow: Bool) {
+        let step: Float = 360.0 / Float(self.buttonsCount)
+        for index in 0..<self.buttonsCount {
+            let button = buttons[index]
+            let angle: Float = Float(index) * step
+            if isShow == true {
+                button.rotatedZ(angle: angle, animated: false)
+                button.showAnimation(distance, duration: 0.3)
+            } else {
+                button.hideAnimation(self.bounds.size.width / 2.0, duration: 0.3)
+            }
+        }
+    }
+}
+
+// MARK: extension
+
+extension Float {
+    var radians: Float {
+        return self * (Float(180) / Float(M_PI))
+    }
+    
+    var degres: Float {
+        return self  * Float(M_PI) / 180.0
+    }
+}
+
+extension UIView {
+    
+    var angleZ: Float {
+        let radians: Float = atan2(Float(self.transform.b), Float(self.transform.a))
+        return radians.radians
+    }
+}
---
+++ CircleMenuLib/CircleMenuButton/CircleMenuButton.swift
@@ -0,0 +1,202 @@
+//
+//  CircleMenuButton.swift
+//  ButtonTest
+//
+//  Created by Alex K. on 18/01/16.
+//  Copyright © 2016 Alex K. All rights reserved.
+//
+
+import UIKit
+
+public class CircleMenuButton: UIButton {
+    
+    // MARK: properties
+    
+    var container: UIView?
+    
+    // MARK: life cicle
+    
+    init(size: CGSize, circleMenu: CircleMenu, distance: Float, angle: Float = 0) {
+        super.init(frame: CGRect(origin: CGPointZero, size: size))
+        
+        self.backgroundColor = UIColor(colorLiteralRed: 0.79, green: 0.24, blue: 0.27, alpha: 1)
+        self.layer.cornerRadius = size.height / 2.0
+        
+        let aContainer = createContainer(CGSize(width: size.width, height:CGFloat(distance)), circleMenu: circleMenu)
+        aContainer.addSubview(self)
+        container = aContainer
+        
+        self.rotatedZ(angle: angle, animated: false)
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: configure
+    
+    private func createContainer(size: CGSize, circleMenu: CircleMenu) -> UIView {
+
+        guard circleMenu.superview != nil else { fatalError("wront circle menu")}
+        
+        let container = Init(UIView(frame:CGRect(origin: CGPointZero, size: size))) {
+            $0.backgroundColor = UIColor.clearColor()
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.layer.anchorPoint = CGPoint(x: 0.5, y: 1)
+        }
+        circleMenu.superview!.insertSubview(container, belowSubview: circleMenu)
+        
+        // added constraints
+        let height = NSLayoutConstraint(item: container,
+                                   attribute: .Height,
+                                   relatedBy: .Equal,
+                                      toItem: nil,
+                                   attribute: .Height,
+                                  multiplier: 1,
+                                    constant: size.height)
+        height.identifier = "height"
+        container.addConstraint(height)
+        
+        container.addConstraint(NSLayoutConstraint(item: container,
+            attribute: .Width,
+            relatedBy: .Equal,
+            toItem: nil,
+            attribute: .Width,
+            multiplier: 1,
+            constant: size.width))
+        
+        circleMenu.superview!.addConstraint(NSLayoutConstraint(item: circleMenu,
+            attribute: .CenterX,
+            relatedBy: .Equal,
+            toItem: container,
+            attribute: .CenterX,
+            multiplier: 1,
+            constant:0))
+        
+        circleMenu.superview!.addConstraint(NSLayoutConstraint(item: circleMenu,
+            attribute: .CenterY,
+            relatedBy: .Equal,
+            toItem: container,
+            attribute: .CenterY,
+            multiplier: 1,
+            constant:0))
+    
+        return container
+    }
+    
+    // MARK: public 
+    
+    public func rotatedZ(angle angle: Float, animated: Bool, duration: Double = 0) {
+        guard container != nil else {fatalError("contaner don't create")}
+        
+        let rotateTransform = CATransform3DMakeRotation(CGFloat(angle.degres), 0, 0, 1)
+        if animated {
+            UIView.animateWithDuration(
+                duration,
+                delay: 0,
+                options: UIViewAnimationOptions.CurveEaseInOut,
+                animations: { () -> Void in
+                    self.container!.layer.transform = rotateTransform
+                },
+                completion: nil)
+        } else {
+            container!.layer.transform = rotateTransform
+        }
+    }
+}
+
+// MARK: Animations
+
+extension CircleMenuButton {
+    
+    public func showAnimation(distance: Float, duration: Double) {
+        let heightConstraint = self.container?.constraints.filter {$0.identifier == "height"}.first
+    
+        guard heightConstraint != nil else {
+            return
+        }
+        self.transform = CGAffineTransformMakeScale(0, 0)
+        self.container?.layoutIfNeeded()
+        
+        self.alpha = 1
+        
+        heightConstraint?.constant = CGFloat(distance)
+        UIView.animateWithDuration(
+            duration,
+            delay: 0,
+            usingSpringWithDamping: 0.6,
+            initialSpringVelocity: 0,
+            options: UIViewAnimationOptions.CurveLinear,
+            animations: { () -> Void in
+                self.container!.layoutIfNeeded()
+                self.transform = CGAffineTransformMakeScale(1, 1)
+            }, completion: { (success) -> Void in
+        })
+    }
+
+    public func hideAnimation(distance: CGFloat, duration: Double, delay: Double = 0) {
+        let heightConstraint = self.container?.constraints.filter {$0.identifier == "height"}.first
+        
+        guard heightConstraint != nil else {
+            return
+        }
+        
+        UIView.animateWithDuration(
+            duration,
+            delay: delay,
+            options: UIViewAnimationOptions.CurveEaseIn,
+            animations: { () -> Void in
+                self.transform = CGAffineTransformMakeScale(0.01, 0.01)
+            }, completion: { (success) -> Void in
+                heightConstraint?.constant = CGFloat(distance)
+                self.alpha = 0
+        })
+    }
+    
+    public func changeDistance(distance: CGFloat, animated: Bool, duration: Double = 0, delay: Double = 0) {
+        let heightConstraint = self.container?.constraints.filter {$0.identifier == "height"}.first
+        
+        guard heightConstraint != nil else {
+            return
+        }
+        
+        heightConstraint?.constant = distance
+        
+        UIView.animateWithDuration(
+            duration,
+            delay: delay,
+            options: UIViewAnimationOptions.CurveEaseIn,
+            animations: { () -> Void in
+                self.container!.layoutIfNeeded()
+            },
+            completion: nil)
+    }
+    
+    public func tapAnimation(byAngle angle: Float, duration: Double, distance: Float) {
+        if let aContainer = container {
+            rotatedZ(angle: aContainer.angleZ + angle, animated: true, duration: duration)
+            changeDistance(self.bounds.size.height / 2.0, animated: true, duration: 0.2, delay: duration - 0.2)
+        }
+    }
+    
+    // MARK: layer animation
+    
+    public func rotationLayerAnimation(angle: Float, duration: Double) {
+        if let aContainer = container {
+            let rotation = Init(CABasicAnimation(keyPath: "transform.rotation")) {
+                $0.duration = NSTimeInterval(duration)
+                $0.toValue = (angle.degres)
+                $0.fillMode = kCAFillModeForwards
+                $0.removedOnCompletion = false
+                $0.delegate = self
+            }
+            aContainer.layer.addAnimation(rotation, forKey: "rotation")
+        }
+    }
+    
+    public override func animationDidStop(anim: CAAnimation, finished flag: Bool) {
+        if let aContainer = container {
+            aContainer.removeFromSuperview()
+        }
+    }
+}
