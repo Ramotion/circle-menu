809667064846eaac11d0a4af27b061bf7c5b46c8
Alex.k <Alex.k@ramotion.com>
2016-01-28 17:01:12 +0300

    include resources

--- CircleMenu/ViewController.swift
+++ CircleMenu/ViewController.swift
@@ -8,9 +8,25 @@
 
 import UIKit
 
+extension UIColor {
+    static func color(red: Int, green: Int, blue: Int, alpha: Float) -> UIColor {
+        return UIColor(
+            colorLiteralRed: Float(1.0) / Float(255.0) * Float(red),
+            green: Float(1.0) / Float(255.0) * Float(green),
+            blue: Float(1.0) / Float(255.0) * Float(blue),
+            alpha: alpha)
+    }
+}
+
 class ViewController: UIViewController, CircleMenuDelegate {
 
-    let colors = [UIColor.redColor(), UIColor.grayColor(), UIColor.greenColor(), UIColor.purpleColor()]
+//    let colors = [UIColor.redColor(), UIColor.grayColor(), UIColor.greenColor(), UIColor.purpleColor()]
+    let items: [(icon: String, color: UIColor)] = [
+                                                    ("icon_back", UIColor.color(234, green: 183, blue: 84, alpha: 1)),
+                                                    ("icon_play", UIColor.color(229, green: 94, blue: 75, alpha: 1)),
+                                                    ("icon_replay", UIColor.color(99, green: 186, blue: 51, alpha: 1)),
+                                                    ("icon_close", UIColor.color(32, green: 164, blue: 203, alpha: 1)),
+                                                  ]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -25,6 +41,7 @@ class ViewController: UIViewController, CircleMenuDelegate {
     // MARK: <CircleMenuDelegate>
     
     func circleMenu(circleMenu: CircleMenu, willDisplay button: CircleMenuButton, atIndex: Int) {
-        button.backgroundColor = colors[atIndex]
+        button.backgroundColor = items[atIndex].color
+        button.setImage(UIImage(imageLiteral: items[atIndex].icon), forState: .Normal)
     }
 }
