3f98e72b7caf40250ba3bf1386e022a07025ca65
Alex.k <Alex.k@ramotion.com>
2016-06-01 17:59:28 +0300

    shared scheme

---
+++ CircleMenu/CircleMenu.h
@@ -0,0 +1,19 @@
+//
+//  CircleMenu.h
+//  CircleMenu
+//
+//  Created by Alex K. on 01/06/16.
+//  Copyright © 2016 Alex K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CircleMenu.
+FOUNDATION_EXPORT double CircleMenuVersionNumber;
+
+//! Project version string for CircleMenu.
+FOUNDATION_EXPORT const unsigned char CircleMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CircleMenu/PublicHeader.h>
+
+
